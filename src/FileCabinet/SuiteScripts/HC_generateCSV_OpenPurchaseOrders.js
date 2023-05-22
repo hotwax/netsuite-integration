9f6991a859c00cf68423e1bd6e642b3476398a75
Rehan Khan <rehan.khan@hotwaxsystems.com>
2023-05-22 23:56:25 +0530

    Added: code to skip CSV file creation if purcahse order saved search doesn't retrun result. (3-push-purchase-orders-from-netsuite-to-oms)

--- src/FileCabinet/SuiteScripts/HC_generateCSV_OpenPurchaseOrders.js
+++ src/FileCabinet/SuiteScripts/HC_generateCSV_OpenPurchaseOrders.js
@@ -4,17 +4,29 @@
  */
 define(['N/task', 'N/search', 'N/record'], function (task, search, record) {
     function execute(context) {
-      var date = new Date();
-      var searchId = 'customsearch_open_purchaserorder'; // Saved Search Id which will export open purchase order
+      try {
+        var date = new Date();
+        var searchId = 'customsearch_open_purchaserorder'; // Saved Search Id which will export open purchase order
   
-      var searchTask = task.create({
-        taskType: task.TaskType.SEARCH
-      });
-  
-      searchTask.savedSearchId = searchId;
+        var savedSearch = search.load({ id: searchId });
+    
+        // Run the search
+        var searchResult = savedSearch.run().getRange({ start: 0, end: 1 });
+      
+        // If the search returned no results, do not create the CSV file
+        if (searchResult.length === 0) {
+          log.debug('No results found. Skipping CSV file creation.');
+          return;
+        }
 
-      // Check Open Purchase Order CSV Folder is created or not 
-      var folderInternalId = search
+        var searchTask = task.create({
+          taskType: task.TaskType.SEARCH
+        });
+  
+        searchTask.savedSearchId = searchId;
+
+        // Check Open Purchase Order CSV Folder is created or not 
+        var folderInternalId = search
           .create({
             type: search.Type.FOLDER,
             filters: [['name', 'is', 'Open Purchase Order CSV']],
@@ -24,26 +36,36 @@ define(['N/task', 'N/search', 'N/record'], function (task, search, record) {
           .getRange({ start: 0, end: 1 })
           .map(function (result) {
             return result.getValue('internalid');
-      })[0];
+        })[0];
       
-      // Made Open Purchase Order CSV folder in NetSuite File Cabinet
-      if (folderInternalId == null) {
+        // Made Open Purchase Order CSV folder in NetSuite File Cabinet
+        if (folderInternalId == null) {
           var folder = record.create({ type: record.Type.FOLDER});
           folder.setValue({ fieldId: 'name',
                   value: 'Open Purchase Order CSV' });
           var folderId = folder.save();
-      }
+        }
   
-      var fileName =  date + '-OpenPurchaseExport.csv';
-      var path = 'Open Purchase Order CSV/' + fileName;
-      searchTask.filePath = path;
+        var fileName =  date + '-OpenPurchaseExport.csv';
+        var path = 'Open Purchase Order CSV/' + fileName;
+        searchTask.filePath = path;
   
-      var searchTaskId = searchTask.submit();
+        var searchTaskId = searchTask.submit();
   
-      var taskStatus = task.checkStatus(searchTaskId);
+        var taskStatus = task.checkStatus(searchTaskId);
 
-      log.debug("Search task is submitted ! " + taskStatus.status);
-      log.debug("Open Purchase Order Fulfillment CSV file Successfully Uploaded in NetSuite with file name ! " + fileName);
+        log.debug("Search task is submitted ! " + taskStatus.status);
+        log.debug("Open Purchase Order Fulfillment CSV file Successfully Uploaded in NetSuite with file name ! " + fileName);
+      } catch (e) {
+        log.error({
+          title: 'Error in generating open purchase order csv files',
+          details: e,
+        });
+        throw error.create({
+          name:"Error in generating open purchase order csv files",
+          message: e
+        });
+      }
     }
     return {
       execute: execute
--- src/FileCabinet/SuiteScripts/HC_uploadCSV_OpenPurchaseOrders.js
+++ src/FileCabinet/SuiteScripts/HC_uploadCSV_OpenPurchaseOrders.js
@@ -75,7 +75,7 @@ define(['N/file', 'N/sftp', 'N/search', 'N/error', 'N/record'], function(file, s
                 keyId: '',
                 url: '',
                 port: 235,
-                directory: '/home/hc-uat-sftp/netsuite/purchaseorder',
+                directory: '/home/{SFTP-USER}/{FOLDER}',
                 hostKey: hostKey
             });
             log.debug("Connection established successfully with SFTP server!");
@@ -108,6 +108,10 @@ define(['N/file', 'N/sftp', 'N/search', 'N/error', 'N/record'], function(file, s
         log.error({
           title: 'Error processing in open purchase order csv files',
           details: e,
+        });
+        throw error.create({
+          name:"Error processing in open purchase order csv files",
+          message: e
         });
       }
     }
