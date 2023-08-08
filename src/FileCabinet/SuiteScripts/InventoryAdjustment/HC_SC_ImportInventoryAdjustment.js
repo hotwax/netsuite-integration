4e34e150a8c1a0887da053345cbabcda417ee407
Rehan Khan <rehan.khan@hotwaxsystems.com>
2023-08-08 14:31:32 +0530

    Updated: Import Inventory Adjustment file and fetch the SFTP credntials from custom record type entity. (32-update-export-feature-as-per-mapreduce-suitescript-from-netsuite-to-oms)

--- src/FileCabinet/SuiteScripts/InventoryAdjustment/HC_SC_ImportInventoryAdjustment.js
+++ src/FileCabinet/SuiteScripts/InventoryAdjustment/HC_SC_ImportInventoryAdjustment.js
@@ -2,7 +2,7 @@
  * @NApiVersion 2.1
  * @NScriptType ScheduledScript
  */
-define(['N/sftp', 'N/task', 'N/error'], function (sftp, task, error) {
+define(['N/sftp', 'N/task', 'N/error', 'N/search'], function (sftp, task, error, search) {
     function execute(context) {
       try {
         // Establish a connection to a remote FTP server
@@ -11,17 +11,60 @@ define(['N/sftp', 'N/task', 'N/error'], function (sftp, task, error) {
           Example: ssh-keyscan -t ECDSA -p 235 hc-uat.hotwax.io 
         */
       
-        var hostKey = '';
-
-        var connection = sftp.createConnection({
-          username: '',
-          keyId: '',
-          url: '',
-          port: 235,
-          directory: '/home/{SFTP-USER}/{FOLDER}',
-          hostKey: hostKey
+        var customRecordSFTPSearch = search.create({
+            type: 'customrecord_ns_sftp_configuration',
+            columns: [
+                'custrecord_ns_sftp_server',
+                'custrecord_ns_sftp_userid',
+                'custrecord_ns_sftp_port_no',
+                'custrecord_ns_sftp_host_key',
+                'custrecord_ns_sftp_guid',
+                'custrecord_ns_sftp_default_file_dir'
+            ]
+            
+        });
+        var sftpSearchResults = customRecordSFTPSearch.run().getRange({
+            start: 0,
+            end: 1
+        });
+   
+        var sftpSearchResult = sftpSearchResults[0];
+      
+        var sftpUrl = sftpSearchResult.getValue({
+            name: 'custrecord_ns_sftp_server'
         });
 
+        var sftpUserName = sftpSearchResult.getValue({
+            name: 'custrecord_ns_sftp_userid'
+        });
+
+        var sftpPort = sftpSearchResult.getValue({
+            name: 'custrecord_ns_sftp_port_no'
+        });
+
+        var hostKey = sftpSearchResult.getValue({
+            name: 'custrecord_ns_sftp_host_key'
+        });
+      
+        var sftpKeyId = sftpSearchResult.getValue({
+            name: 'custrecord_ns_sftp_guid'
+        });
+
+        var sftpDirectory = sftpSearchResult.getValue({
+            name: 'custrecord_ns_sftp_default_file_dir'
+        });
+  
+        sftpDirectory = sftpDirectory + 'inventoryadjustment';
+        sftpPort = parseInt(sftpPort);
+  
+        var connection = sftp.createConnection({
+            username: sftpUserName,
+            keyId: sftpKeyId,
+            url: sftpUrl,
+            port: sftpPort,
+            directory: sftpDirectory,
+            hostKey: hostKey
+        });
         log.debug("Connection established successfully with SFTP server!");
 
         var list = connection.list({
