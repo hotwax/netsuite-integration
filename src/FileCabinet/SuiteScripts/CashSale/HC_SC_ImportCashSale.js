950977dc278eb3445bef2e8428bd28df96343520
Rehan Khan <rehan.khan@hotwaxsystems.com>
2023-08-21 12:46:14 +0530

    Added: support to sync POS sales order from OMS to NetSuite as cash sales. (34-sync-cash-sale-order-or-shopify-pos-orders-from-oms-to-netsuite)

---
+++ src/FileCabinet/SuiteScripts/CashSale/HC_SC_ImportCashSale.js
@@ -0,0 +1,119 @@
+/**
+ * @NApiVersion 2.1
+ * @NScriptType ScheduledScript
+ */
+define(['N/sftp', 'N/task', 'N/error', 'N/search'], function (sftp, task, error, search) {
+    function execute(context) {
+      try {
+        //Get Custom Record Type SFTP details
+        var customRecordSFTPSearch = search.create({
+          type: 'customrecord_ns_sftp_configuration',
+          columns: [
+              'custrecord_ns_sftp_server',
+              'custrecord_ns_sftp_userid',
+              'custrecord_ns_sftp_port_no',
+              'custrecord_ns_sftp_host_key',
+              'custrecord_ns_sftp_guid',
+              'custrecord_ns_sftp_default_file_dir'
+          ]
+          
+        });
+        var sftpSearchResults = customRecordSFTPSearch.run().getRange({
+            start: 0,
+            end: 1
+        });
+  
+        var sftpSearchResult = sftpSearchResults[0];
+        
+        var sftpUrl = sftpSearchResult.getValue({
+            name: 'custrecord_ns_sftp_server'
+        });
+
+        var sftpUserName = sftpSearchResult.getValue({
+            name: 'custrecord_ns_sftp_userid'
+        });
+
+        var sftpPort = sftpSearchResult.getValue({
+            name: 'custrecord_ns_sftp_port_no'
+        });
+
+        var hostKey = sftpSearchResult.getValue({
+            name: 'custrecord_ns_sftp_host_key'
+        });
+        
+        var sftpKeyId = sftpSearchResult.getValue({
+            name: 'custrecord_ns_sftp_guid'
+        });
+
+        var sftpDirectory = sftpSearchResult.getValue({
+            name: 'custrecord_ns_sftp_default_file_dir'
+        });
+
+        sftpDirectory = sftpDirectory + 'cashsale';
+        sftpPort = parseInt(sftpPort);
+
+        var connection = sftp.createConnection({
+            username: sftpUserName,
+            keyId: sftpKeyId,
+            url: sftpUrl,
+            port: sftpPort,
+            directory: sftpDirectory,
+            hostKey: hostKey
+        });
+        log.debug("Connection established successfully with SFTP server!");
+
+        var list = connection.list({
+          path: '/export/'
+        });
+
+        for (var i=0; i<list.length; i++) {
+          if (!list[i].directory) {
+          try {
+            var fileName = list[i].name;
+
+            // Download the file from the remote server
+            var downloadedFile = connection.download({
+              directory: '/export',
+              filename: fileName
+            });
+            log.debug("File downloaded successfully !"+fileName);
+
+            // Create CSV import task
+            var scriptTask = task.create({taskType: task.TaskType.CSV_IMPORT});
+            scriptTask.mappingId = 'custimport_add_cashsale_hc';
+            scriptTask.importFile = downloadedFile;
+            var csvImportTaskId = scriptTask.submit();
+            
+            var taskStatus = task.checkStatus(csvImportTaskId);
+            if (taskStatus.status === 'FAILED') {
+              log.debug("Import Cash Sale CSV task has been failed");
+            } else {
+              connection.move({
+                from: '/export/'+fileName,
+                to: '/export/archive/'+fileName
+              })
+              log.debug('File moved!');
+            }
+          } catch (e) {
+              log.error({
+                title: 'Error in processing cash sale csv files',
+                details: e,
+              });
+          }
+          }
+        }
+      } catch (e) {
+        log.error({
+          title: 'Error in importing cash sale csv files',
+          details: e,
+        });
+        throw error.create({
+          name:"Error in importing cash sale csv files",
+          message: e
+        });
+      }
+   }
+   return {
+     execute: execute
+   };
+});
