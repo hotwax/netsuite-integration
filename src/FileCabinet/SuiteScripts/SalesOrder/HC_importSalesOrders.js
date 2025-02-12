764df2fd151e53600fb304a1abb60a8c28dba1a2
Rehan Khan <131848523+rehan3586@users.noreply.github.com>
2025-02-12 12:52:20 +0530

    Added: required check to skip csv import task execution if CSV file is empty on SFTP server. (188-add-check-to-skip-task-execution-if-csv-file-is-empty) (#189)

--- src/FileCabinet/SuiteScripts/SalesOrder/HC_importSalesOrders.js
+++ src/FileCabinet/SuiteScripts/SalesOrder/HC_importSalesOrders.js
@@ -89,6 +89,7 @@ define(['N/sftp', 'N/task', 'N/error', 'N/search', 'N/file', 'N/runtime'], funct
             });
             log.debug("File downloaded successfully !"+fileName);
 
+            if (downloadedFile && downloadedFile.size > 0) {
             // Create CSV import task
             var scriptTask = task.create({taskType: task.TaskType.CSV_IMPORT});
             scriptTask.mappingId = 'custimport_add_salesorders_hc';
@@ -105,6 +106,13 @@ define(['N/sftp', 'N/task', 'N/error', 'N/search', 'N/file', 'N/runtime'], funct
                 to: '/export/archive/'+fileName
               });
               log.debug('File moved!');
+            }
+            } else {
+              connection.move({
+                from: '/export/'+fileName,
+                to: '/export/archive/'+fileName
+              });
+              log.debug("File is empty, skipping task execution: " + fileName); 
             }
           } catch (e) {
               log.error({
