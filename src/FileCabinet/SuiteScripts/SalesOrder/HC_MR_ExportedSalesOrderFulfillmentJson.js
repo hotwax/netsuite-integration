f5d00627f34ac16ee97962ee6f7b4600d7db2941
aniket12chouhan <aniket12chouhan@gmail.com>
2025-07-01 19:34:30 +0530

    Added: Implemented MapReduce script for exporting Sales Order Fulfillment data to JSON and updated saved search defination add a filter to exclude external type id line item

---
+++ src/FileCabinet/SuiteScripts/SalesOrder/HC_MR_ExportedSalesOrderFulfillmentJson.js
@@ -0,0 +1,274 @@
+/**
+ * @NApiVersion 2.1
+ * @NScriptType MapReduceScript
+ */
+define(['N/file', 'N/record', 'N/search', 'N/sftp', 'N/task', 'N/error'],
+    (file, record, search, sftp, task, error) => {
+        const internalIdList = new Set([]);
+
+        const checkInternalId = (internalid) => {
+            if (internalIdList.has(internalid)) {
+                return false;
+            } else {
+                internalIdList.add(internalid);
+                return true;
+            }
+        }
+
+        const getInputData = (inputContext) => {
+            // Get sales order fulfillment search query
+            var salesOrderFulfillmentSearch = search.load({ id: 'customsearch_hc_export_so_fulfillment' });
+            return salesOrderFulfillmentSearch;
+        }
+
+        const map = (mapContext) => {
+            var contextValues = JSON.parse(mapContext.value);
+
+            var orderId = contextValues.values.custbody_hc_order_id;
+            var externalId = contextValues.values["externalid.createdFrom"].value;
+            var orderItemSeqId = contextValues.values.custcol_hc_order_line_id;
+            var externalFacilityId = contextValues.values.location.value;
+            var shippedDate = contextValues.values.formulatext;
+            var quantity = contextValues.values.quantity;
+            var trackingNumber = contextValues.values.trackingnumbers;
+            if (trackingNumber && trackingNumber.includes("<BR>")) {
+                trackingNumber = trackingNumber.replaceAll('<BR>', ' | ');
+            }
+            var shippingCarrier = contextValues.values.shipcarrier.text;
+            var shippingMethod = contextValues.values.shipmethod.text;
+            var shippingMethodId = contextValues.values.shipmethod.value;
+            var fulfillmentInternalId = contextValues.id;
+
+            var shipmentData = {
+                'orderId': orderId ? orderId : externalId,
+                'orderItemSeqId': orderItemSeqId,
+                'externalFacilityId': externalFacilityId,
+                'shippedDate': shippedDate,
+                'quantity': quantity,
+                'trackingNumber': trackingNumber,
+                'shippingCarrier': shippingCarrier,
+                'shippingMethod': shippingMethod,
+                'shippingMethodId': shippingMethodId
+            };
+            if (fulfillmentInternalId) {
+                var checkId = checkInternalId(fulfillmentInternalId);
+                if (checkId) {
+                    var id = record.submitFields({
+                        type: record.Type.ITEM_FULFILLMENT,
+                        id: fulfillmentInternalId,
+                        values: {
+                            custbody_hc_fulfillment_exported: true
+                        }
+                    });
+                }
+            }
+
+            mapContext.write({
+                key: fulfillmentInternalId,
+                value: shipmentData
+            });
+        }
+
+        const reduce = (reduceContext) => {
+            var orderMap = {
+                items: [],
+            };
+            reduceContext.values.forEach((val) => {
+                const item = JSON.parse(val);
+                if (!orderMap.orderId) {
+                    orderMap = {
+                        orderId: item.orderId,
+                        items: []
+                    };
+                }
+
+                orderMap.items.push({
+                    orderItemSeqId: item.orderItemSeqId,
+                    externalFacilityId: item.externalFacilityId,
+                    shippedDate: item.shippedDate,
+                    quantity: item.quantity,
+                    trackingNumber: item.trackingNumber,
+                    carrier: item.shippingCarrier,
+                    shippingMethod: item.shippingMethod,
+                    "validation-result": "success"
+                });
+            });
+
+            var fulfilledSalesOrderItem = {
+                orderMap: orderMap,
+            }
+            reduceContext.write({
+                key: reduceContext.key,
+                value: JSON.stringify(fulfilledSalesOrderItem)
+            });
+        }
+
+        const summarize = (summaryContext) => {
+            try {
+                let result = [];
+                var totalRecordsExported = 0;
+
+                summaryContext.output.iterator().each(function (key, value) {
+                    result.push(JSON.parse(value));
+                    totalRecordsExported = totalRecordsExported + 1;
+                    return true;
+                });
+
+                log.debug("====totalRecordsExported==" + totalRecordsExported);
+                if (totalRecordsExported > 0) {
+                    var fileName = 'SalesOrderFulfillment-' + summaryContext.dateCreated.toISOString().replace(/[:T]/g, '-').replace(/\..+/, '') + '.json';
+                    var fileObj = file.create({
+                        name: fileName,
+                        fileType: file.Type.JSON,
+                        contents: JSON.stringify(result, null, 2),
+                        encoding: file.Encoding.UTF_8
+                    });
+
+                    // Establish a connection to a remote FTP server
+                    /* The host key can be obtained using OpenSSH's ssh-keyscan tool:
+                    ssh-keyscan -t <hostKeyType> -p <port> <hostDomain>
+                    Example: ssh-keyscan -t ECDSA -p 235 hc-uat.hotwax.io 
+                    */
+
+                    //Get Custom Record Type SFTP details
+                    var customRecordSFTPSearch = search.create({
+                        type: 'customrecord_ns_sftp_configuration',
+                        columns: [
+                            'custrecord_ns_sftp_server',
+                            'custrecord_ns_sftp_userid',
+                            'custrecord_ns_sftp_port_no',
+                            'custrecord_ns_sftp_host_key',
+                            'custrecord_ns_sftp_guid',
+                            'custrecord_ns_sftp_default_file_dir'
+                        ]
+
+                    });
+                    var sftpSearchResults = customRecordSFTPSearch.run().getRange({
+                        start: 0,
+                        end: 1
+                    });
+
+                    var sftpSearchResult = sftpSearchResults[0];
+
+                    var sftpUrl = sftpSearchResult.getValue({
+                        name: 'custrecord_ns_sftp_server'
+                    });
+                    var sftpUserName = sftpSearchResult.getValue({
+                        name: 'custrecord_ns_sftp_userid'
+                    });
+
+                    var sftpPort = sftpSearchResult.getValue({
+                        name: 'custrecord_ns_sftp_port_no'
+                    });
+
+                    var hostKey = sftpSearchResult.getValue({
+                        name: 'custrecord_ns_sftp_host_key'
+                    });
+
+                    var sftpKeyId = sftpSearchResult.getValue({
+                        name: 'custrecord_ns_sftp_guid'
+                    });
+
+                    var sftpDirectory = sftpSearchResult.getValue({
+                        name: 'custrecord_ns_sftp_default_file_dir'
+                    });
+
+                    sftpDirectory = sftpDirectory + 'salesorder';
+                    sftpPort = parseInt(sftpPort);
+
+                    var connection = sftp.createConnection({
+                        username: sftpUserName,
+                        secret: sftpKeyId,
+                        url: sftpUrl,
+                        port: sftpPort,
+                        directory: sftpDirectory,
+                        hostKey: hostKey
+                    });
+                    log.debug("Connection established successfully with SFTP server!");
+
+                    if (fileObj.size > connection.MAX_FILE_SIZE) {
+                        throw error.create({
+                            name: "FILE_IS_TOO_BIG",
+                            message: "The file you are trying to upload is too big"
+                        });
+                    }
+                    connection.upload({
+                        directory: '/import/fulfillment/',
+                        file: fileObj
+                    });
+                    log.debug("Sales Order Fulfillment CSV File Uploaded Successfully to SFTP server with file" + fileName);
+                }
+            } catch (e) {
+                //Generate error csv
+                var errorFileLine = 'orderId,Recordtype\n';
+
+                summaryContext.output.iterator().each(function (key, value) {
+                    var index = key.split('-')
+                    var internalId = index[0];
+                    var recordType = "ITEM_FULFILLMENT";
+
+                    var valueContents = internalId + ',' + recordType + '\n';
+                    errorFileLine += valueContents;
+
+                    return true;
+                });
+
+                var fileName = summaryContext.dateCreated + '-FailedSalesOrderFulfillmentExport.csv';
+                var failExportCSV = file.create({
+                    name: fileName,
+                    fileType: file.Type.CSV,
+                    contents: errorFileLine
+                });
+
+                // Check HotWax Export Fail Record CSV is created or not
+                var folderInternalId = search
+                    .create({
+                        type: search.Type.FOLDER,
+                        filters: [['name', 'is', 'HotWax Export Fail Record CSV']],
+                        columns: ['internalid']
+                    })
+                    .run()
+                    .getRange({ start: 0, end: 1 })
+                    .map(function (result) {
+                        return result.getValue('internalid');
+                    })[0];
+
+                // Made Export Fail Sales Order CSV folder in NetSuite File Cabinet
+                if (folderInternalId == null) {
+                    var folder = record.create({ type: record.Type.FOLDER });
+                    folder.setValue({
+                        fieldId: 'name',
+                        value: 'HotWax Export Fail Record CSV'
+                    });
+
+                    var folderInternalId = folder.save();
+                }
+
+                failExportCSV.folder = folderInternalId;
+                failExportCSV.save();
+
+                if (folderInternalId) {
+                    var scriptTask = task.create({
+                        taskType: task.TaskType.MAP_REDUCE,
+                    });
+
+                    scriptTask.scriptId = 'customscript_hc_mr_mark_false',
+                        scriptTask.deploymentId = 'customdeploy_hc_mr_mark_false'
+                    scriptTask.params = { "custscript_hc_mr_mark_false": folderInternalId }
+
+                    var mapReduceTaskId = scriptTask.submit();
+                    log.debug("Map/reduce task submitted!");
+                }
+
+                log.error({
+                    title: 'Error in exporting and uploading sales order fulfillment csv files',
+                    details: e,
+                });
+                throw error.create({
+                    name: "Error in exporting and uploading sales order fulfillment csv files",
+                    message: e
+                });
+            }
+        }
+        return { getInputData, map, reduce, summarize }
+    });
