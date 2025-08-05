4b8bf85920480c826727738e13398f99fdacf45e
Gurveen Kaur <gurveen.bagga@hotwax.co>
2025-08-05 16:57:19 +0530

    Improved: Set HC Order ID to empty for TO Make Copy action

--- src/FileCabinet/SuiteScripts/TransferOrderV2/HC_CS_TO_Copy_Reset_HC_Ord_Exp_v2.js
+++ src/FileCabinet/SuiteScripts/TransferOrderV2/HC_CS_TO_Copy_Reset_HC_Ord_Exp_v2.js
@@ -4,23 +4,28 @@
  */
 define([], () => {
 
-  function pageInit(context) {
-    const currentRecord = context.currentRecord;
-    const mode = context.mode;
+   function pageInit(context) {
+      const currentRecord = context.currentRecord;
+      const mode = context.mode;
 
-    if (mode === 'copy') {
-      const tranId = currentRecord.getValue({ fieldId: 'tranid' }) || '[Not Assigned Yet]';
+      if (mode === 'copy') {
+         const tranId = currentRecord.getValue({ fieldId: 'tranid' }) || '[Not Assigned Yet]';
 
-      console.debug('Resetting Field', `Unchecking 'HC Order Exported' for TO: ${tranId}`);
+         // Uncheck the "HC Order Exported" checkbox
+         currentRecord.setValue({
+            fieldId: 'custbody_hc_order_exported',
+            value: false
+         });
 
-      currentRecord.setValue({
-        fieldId: 'custbody_hc_order_exported',
-        value: false
-      });
-    }
-  }
+         // Clear the "HC Order ID" field (free-form text)
+         currentRecord.setValue({
+            fieldId: 'custbody_hc_order_id',
+            value: ''
+         });
+      }
+   }
 
-  return {
-    pageInit
-  };
+   return {
+      pageInit
+   };
 });
